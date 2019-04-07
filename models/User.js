5901ea7f0e4f6c004c5479691ab2644960b54fb8
Julian Alexander <julian.pineyro@gmail.com>
2019-04-07 23:55:57 +0100

    Crated Login with User and Password validation

--- models/User.js
+++ models/User.js
@@ -7,6 +7,10 @@ const UserSchema = new Schema({
         type: String,
         require: true
     },
+    email: {
+        type: String,
+        require: true
+    },
     password: {
         type: String,
         required: true
