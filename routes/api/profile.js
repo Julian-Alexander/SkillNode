a6cce6220a3aefe162bddabad49ee87eab7eba10
Julian Alexander <julian.pineyro@gmail.com>
2019-04-08 03:47:45 +0100

    Implemented finding all profiles and profiles by handle and id with validation check

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -27,7 +27,7 @@ router.get(
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
-      .populate("user", [ "name", "avatar"])
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
@@ -38,6 +38,65 @@ router.get(
       .catch(err => res.status(404).json(err));
   }
 );
+
+// @route GET api/profile/all
+// @desc Get all profiles
+// @access Public
+router.get("/all", (req, res) => {
+    const errors = {};
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = "There are no profiles";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ profile: "There are no profiles" }));
+});
+
+// @route GET api/profile/handle/:handle
+// @desc Get profile by handle
+// @access Public
+
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route GET api/profile/user/:user_od
+// @desc Get profile by user ID
+// @access Public
+
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    //.catch(err => res.status(404).json(err));  //Mongoose error
+    .catch(err =>
+      res.status(404).json({ profile: "There is no profile for this user" })
+    );
+});
 
 // @route POST api/profile
 // @desc Create user profile
@@ -80,8 +139,7 @@ router.post(
     if (req.body.pinterest) profileFields.social.pinterest = req.body.pinterest;
     if (req.body.snapchat) profileFields.social.snapchat = req.body.snapchat;
 
-    Profile.findOne({ user: req.user.id })
-    .then(profile => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
       if (profile) {
         // Update
         Profile.findOneAndUpdate(
