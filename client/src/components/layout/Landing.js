ed3f8dda7c5f7ad074c28a84e50e7b3670048673
Julian Alexander <julian.pineyro@gmail.com>
2019-04-08 09:41:50 +0100

    React router setup

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+
 
 class Landing extends Component {
   render() {
@@ -15,12 +17,12 @@ class Landing extends Component {
                   members
                 </p>
                 <hr />
-                <a href="register.html" class="btn btn-lg btn-info mr-2">
+                <Link to="/register" class="btn btn-lg btn-info mr-2">
                   Sign Up
-                </a>
-                <a href="login.html" class="btn btn-lg btn-light">
+                </Link>
+                <Link to="/login" class="btn btn-lg btn-light">
                   Login
-                </a>
+                </Link>
               </div>
             </div>
           </div>
