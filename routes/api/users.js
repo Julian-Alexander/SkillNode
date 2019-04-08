20cc9c3d95160600018d19623b13104e7f12862c
Julian Alexander <julian.pineyro@gmail.com>
2019-04-08 01:11:49 +0100

    Implemented validation handlers for registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+
 // Load User model
 const User = require("../../models/User");
 
@@ -14,10 +17,17 @@ const User = require("../../models/User");
 // @access Public
 router.get("/test", (req, res) => res.json({ msg: "Users Works!" }));
 
-// @route GET api/users/test
-// @desc Tests users route
+// @route GET api/users/register
+// @desc Register user
 // @access Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+  
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
@@ -99,9 +109,9 @@ router.get(
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     res.json({
-        id: req.user.id,
-        name: req.user.name,
-        email: req.user.email
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
     });
     //res.json({ msg: "Success" }); // Test connection successful
   }
