24c5b59ad80bff9d0e4473e2c53bc0e501ca95d2
Julian Alexander <julian.pineyro@gmail.com>
2019-04-08 01:40:19 +0100

    Implemented validation handlers for login

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 
 // Load Input Validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 // Load User model
 const User = require("../../models/User");
@@ -24,10 +25,10 @@ router.post("/register", (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
   // Check Validation
-  if(!isValid) {
+  if (!isValid) {
     return res.status(400).json(errors);
   }
-  
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
@@ -62,6 +63,13 @@ router.post("/register", (req, res) => {
 // @desc Login User / Returning JWT Token
 // @access Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -69,7 +77,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+        errors.email = "User not found"
+      return res.status(404).json(errors);
     }
 
     // Check Password
@@ -95,7 +104,8 @@ router.post("/login", (req, res) => {
         //From past API logic text
         // res.json({ msg: "Success" });
       } else {
-        return res.status(400).json({ password: "Password incorrect" });
+          errors.password = "Password incorrect"
+        return res.status(400).json(errors);
       }
     });
   });
