1c584f3ad76ea25439336b31ea5267f372408fbe
Julian Alexander <julian.pineyro@gmail.com>
2019-04-10 02:43:59 +0100

    Added get posts action and post feed component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,11 +2,23 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import PostForm from "./PostForm";
+import PostFeed from "./PostFeed";
 import Spinner from "../common/Spinner";
-
+import { getPosts } from "../../actions/postActions";
 
 class Posts extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
   render() {
+    const { posts, loading } = this.props.post;
+    let postContent;
+
+    if (posts === null || loading) {
+      postContent = <Spinner />;
+    } else {
+      postContent = <PostFeed posts={posts} />;
+    }
     return (
       <div className="feed">
         <div className="container">
@@ -21,7 +33,16 @@ class Posts extends Component {
   }
 }
 
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
 
+const mapStateToProps = state => ({
+  post: state.post
+});
 
-
-export default Posts;
+export default connect(
+  mapStateToProps,
+  { getPosts }
+)(Posts);
