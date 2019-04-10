66bcbf4f26631a916119d4e31a5bf8243fbdcbee
Julian Alexander <julian.pineyro@gmail.com>
2019-04-10 05:02:47 +0100

    Applied env variables

---
+++ client/src/config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    client_id: "4003f7aed0017967c8f1",
+    client_secret: "f3ad02c7e38fef7a27c3e33a5a87b017dce900e4"
+  };
+  
---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI:
+      "mongodb://JulianAlexander:database123@ds233596.mlab.com:33596/snet",
+    secretOrKey: "secret",
+  };
+  
